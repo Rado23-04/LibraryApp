bae052f039d0c7fbd998af2461edaa1880132992
rado <hei.rado.2@gmail.com>
2023-11-19 15:22:18 +0300

    feat: create files DAO suscriber

--- src/Repository/CrudOperationSuscribers.java
+++ src/Repository/CrudOperationSuscribers.java
@@ -1,4 +1,28 @@
 package Repository;
 
-public class CrudOperationSuscribers {
+import Modal.Suscriber;
+
+import java.util.List;
+
+public class CrudOperationSuscribers implements CrudOperation <Suscriber>{
+
+    @Override
+    public List<Suscriber> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Suscriber> saveAll(List<Suscriber> toSave) {
+        return null;
+    }
+
+    @Override
+    public Suscriber save(Suscriber toSave) {
+        return null;
+    }
+
+    @Override
+    public Suscriber delete(Suscriber toDelete) {
+        return null;
+    }
 }
