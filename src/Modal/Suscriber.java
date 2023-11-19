779bb33826ee668ac22829116d8cffdd3dc7d59f
rado <hei.rado.2@gmail.com>
2023-11-19 15:20:20 +0300

    feat create file suscriber

---
+++ src/Modal/Suscriber.java
@@ -0,0 +1,9 @@
+package Modal;
+
+public class Suscriber extends User{
+
+    public Suscriber(int id, String name, String last_name, String email, String sexe) {
+        super(id, name, last_name, email, sexe);
+    }
+
+}
