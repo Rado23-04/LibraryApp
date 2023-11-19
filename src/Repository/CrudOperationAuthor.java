352184faabb65d493b5950e6c0ce3bfed5fa15c1
rado <hei.rado.2@gmail.com>
2023-11-19 14:33:40 +0300

    feat: added DAO to Author

--- src/Repository/CrudOperationAuthor.java
+++ src/Repository/CrudOperationAuthor.java
@@ -3,7 +3,7 @@ package Repository;
 import ConnectionDatabase.ConnectionDatabase;
 import Modal.Author;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 public class CrudOperationAuthor implements CrudOperation <Author> {
@@ -15,23 +15,76 @@ public class CrudOperationAuthor implements CrudOperation <Author> {
     @Override
     public List<Author> findAll() {
         getConnection();
-
-
+    try {
+        String sql = "SELECT * FROM Author";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        System.out.printf("List of Author");
+        while (resultSet.next()){
+            int id = resultSet.getInt("author_id");
+            String name = resultSet.getString("name");
+            int birthday = resultSet.getInt("birth_year");
+            System.out.println(
+                    "Author { id : "+id+
+                            ", name : "+name+
+                            ", birthday: "+birthday+
+                            "};"
+            );
+        }
+    }catch (SQLException e){
+        throw new RuntimeException(e);
+    }
         return null;
     }
 
     @Override
-    public List<Author> saveAll(List<Author> toSave) {
+    public List<Author> saveAll(List<Author> author)  {
+        getConnection();
+        try {
+            for (Author authors:author){
+                String sql = "INSERT INTO Author (name, first_name, sex) VALUES (?,?,?)";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setInt(1,authors.getId());
+                statement.setString(2,authors.getName());
+                statement.setInt(2,authors.getBirthday());
+                int row = statement.executeUpdate();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Add list of  Authors successfully !");
         return null;
     }
 
     @Override
-    public Author save(Author toSave) {
+    public Author save(Author author){
+        getConnection();
+        try{
+            String sql = "INSERT INTO Author (name, birth_year) VALUES (?,?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1,author.getName());
+            statement.setInt(2, author.getBirthday());
+            int row = statement.executeUpdate();
+            System.out.println("Add Author successfully !");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
-    public Author delete(Author toDelete) {
+    public Author delete(Author toDelete)  {
+        getConnection();
+        try {
+            String sql = "delete from author where author_id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1,toDelete.getId());
+            int row = statement.executeUpdate();
+            System.out.println("Deleted Author successfully !");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 }
